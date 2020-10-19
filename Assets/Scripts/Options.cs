1c524733a1f7ffff6d55f7b207929ec8175b2930
mvollstagg <ronarko1996@gmail.com>
2020-10-20 02:04:13 +0300

    BugFixes
    -Ana menü tasarımı ve müziği
    -Player duvardan çıkma
    -Ses açma kapama

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    public AudioSource m_MyAudioSource;
+    //Value from the slider, and it converts to volume level
+    float m_MySliderValue;
+    public Slider musicVolumeSlider;
+
+    void Update()
+    {
+    	m_MyAudioSource.volume = musicVolumeSlider.value;
+        //Displays the value of the slider in the console.
+        Debug.Log(musicVolumeSlider.value);
+    }
+}
