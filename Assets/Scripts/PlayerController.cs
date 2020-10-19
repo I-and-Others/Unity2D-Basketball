69e504014779e67144a5a514a598ffa02533dce6
mvollstagg <ronarko1996@gmail.com>
2020-10-19 03:18:21 +0300

    Proje Oluşturuldu

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{    
+    public VariableJoystick variableJoystick;
+	[SerializeField] private float movementSpeed;
+	//[SerializeField] private Animator anim;
+	private Vector2 movementDirection;
+	private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Time.timeScale = 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processDirection();
+        //processAnimation();
+        
+    }
+
+    private void FixedUpdate(){
+    	move();
+    }
+
+    private void processDirection(){
+    	movementDirection = new Vector2(variableJoystick.Horizontal,variableJoystick.Vertical);
+
+    }
+
+    private void move(){
+    	rb.velocity = movementDirection.normalized * movementSpeed * Time.deltaTime;
+    }
+
+    /*private void processAnimation(){
+    	anim.SetFloat("Horizontal", movementDirection.x);
+    	anim.SetFloat("Vertical", movementDirection.y);
+    }*/
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScoreManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int Score;
+    public Text ScoreText;
+    public AudioSource basketSesi;
+    public GameObject yourButton;
+    void Start()
+    {
+        basketSesi = GetComponent<AudioSource> ();
+    }
+    // Update is called once per frame
+    void Update(){
+        if(Score >= 5){
+            YouWin();
+        }
+        
+    }
+    void YouWin(){
+        ScoreText.text = "You Win!";
+        Time.timeScale = 0f;
+        yourButton.SetActive(true);
+    }
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag =="Ball"){
+			Destroy(other.gameObject);
+			AddScore();
+        }        
+    }
+
+    void AddScore(){
+        Score++;
+        ScoreText.text = Score.ToString();
+        basketSesi.Play();
+    }
+}
