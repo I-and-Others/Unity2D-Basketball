b4670007f5c90437b4016c630bb410397902a491
bilalcinal <73084109+bilalcinal@users.noreply.github.com>
2020-10-20 03:29:18 +0300

    V.0.0.6 Geliştirmeleri
    -Oyun içi ayar menü eklendi
    -Ayar menüsüne müzik,oyuna return butonu ve ana menüye dönüş butonu eklendi
    -Arayüz değiştirildi
    -Oyun içi müzik eklendi

---
+++ Assets/Scripts/InGameOptions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class InGameOptions : MonoBehaviour
+{
+
+    public AudioSource m_MyAudioSource;
+    //Value from the slider, and it converts to volume level
+    float m_MySliderValue;
+    public Slider musicVolumeSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_MyAudioSource.volume = musicVolumeSlider.value;
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;        
+    }
+    public void SettingsOn()
+    {
+        Time.timeScale = 0;
+
+    }
+}
